ee583ecda53ff4a67316928b865b55a138938f67
atulsharma96-ops <sharmaatul9555@gmail.com>
2025-12-23 11:52:05 +0530

    Some miner changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -24,6 +24,7 @@ const Card = () => {
     return (
         <>
             <div className='bg-gray-600 p-5 rounded-xl'>
+                <h2 className='bg-slate-50 p-5 rounded-xl text-black font-semibold text-3xl mb-3'>React First Peoject</h2>
                 <h2 className='bg-red-600 p-3 rounded-xl text-2xl'>Welcome to Tailwindcss</h2>
                 <p className='bg-gray-200 text-black p-3 font-semibold text-xl mt-3 mb-3 rounded-md'>Count = {count}</p>
                 <button onClick={addVal} className='bg-green-600 m-3 border-none outline-none hover:bg-green-700 transition-all duration-300 hover:border-none focus:outline-none'>Add Counter</button>
